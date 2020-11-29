0f0170794f6a7ec5fc71c831caedad75017f6bad
Robin Schroer <git@sulami.xyz>
2020-11-29 14:49:39 +0100

    Use an empty default db

--- src/cljs/browniepoints/db.cljs
+++ src/cljs/browniepoints/db.cljs
@@ -1,7 +1,4 @@
 (ns browniepoints.db)
 
 (def default-db
-  {:people {:robin {:name "Robin"
-                    :points 2}
-            :helene {:name "Hélène"
-                     :points 6}}})
+  {:people {}})
